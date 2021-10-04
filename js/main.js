1e56e450753364fb02514056c73a846dbc8b877b
Ghiftee <gigibetine@gmail.com>
2021-10-04 17:02:25 +0100

    Hide menu when buttin us clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 const showNav = () => {
   document.getElementById('mobile-nav').style.display = 'flex';
 };
+
+const hideNav = () => {
+  document.getElementById('mobile-nav').style.display = 'none';
+};
